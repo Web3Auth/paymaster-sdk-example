4b8eca0631cb0368e4ed5f06925ac61d3a6916f5
lwin <lwin@tor.us>
2024-11-12 04:19:18 +0800

    handle mint error

--- src/components/external-sponsor.tsx
+++ src/components/external-sponsor.tsx
@@ -1,52 +1,70 @@
-'use client'
+"use client";
 
-import { SOURCE_CHAIN, WEB3PAY_API_URL } from "@/config"
-import { useCallback, useEffect, useState } from "react"
-import { Address, generatePrivateKey, PrivateKeyAccount, privateKeyToAccount } from "viem/accounts"
+import { SOURCE_CHAIN, WEB3PAY_API_URL } from "@/config";
+import { useCallback, useEffect, useState } from "react";
+import {
+  Address,
+  generatePrivateKey,
+  PrivateKeyAccount,
+  privateKeyToAccount,
+} from "viem/accounts";
 
 interface ExternalSponsorProps {
-  onEoaWalletFunded: (account: PrivateKeyAccount) => void
+  onEoaWalletFunded: (account: PrivateKeyAccount) => void;
 }
 
-export default function ExternalSponsor({ onEoaWalletFunded }: ExternalSponsorProps) {
-  const [loadingText, setLoadingText] = useState('Generating random EOA wallet...')
-  const [eoaWallet, setEoaWallet] = useState<PrivateKeyAccount>()
+export default function ExternalSponsor({
+  onEoaWalletFunded,
+}: ExternalSponsorProps) {
+  const [loadingText, setLoadingText] = useState(
+    "Generating random EOA wallet..."
+  );
+  const [eoaWallet, setEoaWallet] = useState<PrivateKeyAccount>();
 
   const fundEoaWallet = useCallback(async (eoaAddress: Address) => {
     await fetch(`${WEB3PAY_API_URL}/mint`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ chainId: SOURCE_CHAIN.id, toAddress: eoaAddress }),
-    })
-  }, [])
-  
+    });
+  }, []);
+
   useEffect(() => {
-    let timeout: NodeJS.Timeout
+    let timeout: NodeJS.Timeout;
     (async () => {
-      setLoadingText('Generating random EOA wallet...')
+      setLoadingText("Generating random EOA wallet...");
       timeout = setTimeout(async () => {
-        const privateKey = generatePrivateKey()
-        const account = privateKeyToAccount(privateKey)
-        setEoaWallet(account)
+        try {
+          const privateKey = generatePrivateKey();
+          const account = privateKeyToAccount(privateKey);
+          setEoaWallet(account);
 
-        // funding test token to eoa wallet
-        setLoadingText('Funding test token to EOA wallet...')
+          // funding test token to eoa wallet
+          setLoadingText("Funding test token to EOA wallet...");
 
-        await fundEoaWallet(account.address)
+          await fundEoaWallet(account.address);
 
-        setLoadingText('EOA wallet funded!')
-        onEoaWalletFunded(account)
-      }, 1000)
-    })()
-    return () => clearTimeout(timeout)
-  }, [fundEoaWallet, onEoaWalletFunded])
+          setLoadingText("EOA wallet funded!");
+          onEoaWalletFunded(account);
+        } catch (error) {
+          console.error(error);
+          setLoadingText("Failed to fund EOA wallet");
+        }
+      }, 1000);
+    })();
+    return () => clearTimeout(timeout);
+  }, [fundEoaWallet, onEoaWalletFunded]);
 
   return (
     <div className="flex flex-col items-center justify-center gap-3 border border-gray-300 rounded-md p-6">
-      {eoaWallet && <p className="text-sm bg-gray-100 p-2 rounded-md">EOA address: {eoaWallet.address}</p>}
+      {eoaWallet && (
+        <p className="text-sm bg-gray-100 p-2 rounded-md">
+          EOA address: {eoaWallet.address}
+        </p>
+      )}
       <p className="text-xs">{loadingText}</p>
     </div>
-  )
+  );
 }
