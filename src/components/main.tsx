167e4a799efbd8130325eeacb172c7dc0b82f795
weechien <83656073+huggingbot@users.noreply.github.com>
2025-02-20 18:33:44 +0800

    refactor: Refactor crosschain transaction flow with prepare and execute steps

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { MultiChainAccount } from "@web3auth/chain-abstraction-sdk";
+import { MultiChainAccount, Web3AuthPrepareMultiChainUserOperationReturnType } from "@web3auth/chain-abstraction-sdk";
 import { SignerType } from "@web3auth/erc7579";
 import { useEffect, useState } from "react";
 import { Address, Hex, zeroAddress } from "viem";
@@ -20,7 +20,7 @@ interface WalletProps {
 
 export default function Main({ account, type = SignerType.WEBAUTHN }: WalletProps) {
   const [loading, setLoading] = useState(true);
-  const [loadingText, setLoadingText] = useState('Loading...');
+  const [loadingText, setLoadingText] = useState("Loading...");
   const [accountAddress, setAccountAddress] = useState<Address>(zeroAddress);
   const [txType, setTxType] = useState<CrosschainTransactionType>(CrosschainTransactionType.CROSSCHAIN_SPONSORSHIP);
   const [txModalOpen, setTxModalOpen] = useState(false);
@@ -28,20 +28,21 @@ export default function Main({ account, type = SignerType.WEBAUTHN }: WalletProp
   const [fundModalOpen, setFundModalOpen] = useState(true);
   const [finalUserOpHash, setFinalUserOpHash] = useState<Hex | undefined>(undefined);
   const [blockExplorerUrl, setBlockExplorerUrl] = useState<string | undefined>(undefined);
+  const [preparedTxDetails, setPreparedTxDetails] = useState<
+    | (Web3AuthPrepareMultiChainUserOperationReturnType & {
+        sourceChainIds: number[];
+        targetChainId: number;
+      })
+    | null
+  >(null);
 
   async function handleButtonClick(txType: CrosschainTransactionType) {
     setTxType(txType);
     setTxModalOpen(true);
   }
 
-  async function handleTxExecute(
-    sourceChainIds: number[],
-    targetChainId: number,
-    sourceFunds?: Hex[]
-  ) {
-    setTxModalOpen(false);
+  async function handlePrepareTx(sourceChainIds: number[], targetChainId: number, sourceFunds?: Hex[]) {
     setLoading(true);
-
     try {
       const paymaster = await initWeb3AuthPaymaster();
       const parsedCrossChainUserOpParams = parseCrosschainUserOpData({
@@ -52,37 +53,57 @@ export default function Main({ account, type = SignerType.WEBAUTHN }: WalletProp
         sourceFunds,
       });
 
-      setLoadingText('Preparing user operation...');
-      const { estimatedGasFeesOnTargetChain, ...preparedMultiChainUserOperation } = await prepareCrosschainUserOp({
+      setLoadingText("Preparing user operation...");
+      const preparedOp = await prepareCrosschainUserOp({
         ...parsedCrossChainUserOpParams,
         paymaster,
         account,
       });
-      console.log("estimatedGasFeesOnTargetChain", estimatedGasFeesOnTargetChain);
-      console.log("preparedMultiChainUserOperation", preparedMultiChainUserOperation);
 
-      setLoadingText('Executing crosschain user operation...');
-      const { sourceUserOperationHashes, targetUserOperationHash } = await account.sendMultiChainUserOperation(preparedMultiChainUserOperation);
-      console.log('sourceUserOperationHashes', sourceUserOperationHashes);
-
-      setLoadingText("Waiting for source op receipts...");
-      const receipts = await Promise.all(sourceUserOperationHashes.map((op) => {
-        const bundlerClient = getBundlerClient(op.chainId);
-        return bundlerClient.waitForUserOperationReceipt({ hash: op.userOperationHash });
-      }))
-      console.log('source op receipts', receipts);
-
-      setLoadingText("Waiting for target op receipt...");
-      const targetBundlerClient = getBundlerClient(targetChainId);
-      const targetReceipt = await targetBundlerClient.waitForUserOperationReceipt({ hash: targetUserOperationHash });
-      console.log('targetReceipt', targetReceipt);
-
-      setBlockExplorerUrl(getBlockExplorerUrl(targetChainId));
-      setFinalUserOpHash(targetReceipt.receipt.transactionHash);
+      setPreparedTxDetails({
+        ...preparedOp,
+        sourceChainIds,
+        targetChainId,
+      });
     } catch (error) {
-      console.error('Error executing crosschain user operation', error);
+      console.error("Error preparing crosschain user operation", error);
     } finally {
       setLoading(false);
+    }
+  }
+
+  async function handleTxExecute() {
+    if (!preparedTxDetails) return;
+
+    setTxModalOpen(false);
+    setLoading(true);
+
+    try {
+      setLoadingText("Executing crosschain user operation...");
+      const { sourceUserOperationHashes, targetUserOperationHash } = await account.sendMultiChainUserOperation(preparedTxDetails);
+      console.log("sourceUserOperationHashes", sourceUserOperationHashes);
+
+      setLoadingText("Waiting for source op receipts...");
+      const receipts = await Promise.all(
+        sourceUserOperationHashes.map((op) => {
+          const bundlerClient = getBundlerClient(op.chainId);
+          return bundlerClient.waitForUserOperationReceipt({ hash: op.userOperationHash });
+        })
+      );
+      console.log("source op receipts", receipts);
+
+      setLoadingText("Waiting for target op receipt...");
+      const targetBundlerClient = getBundlerClient(preparedTxDetails.targetChainId);
+      const targetReceipt = await targetBundlerClient.waitForUserOperationReceipt({ hash: targetUserOperationHash });
+      console.log("targetReceipt", targetReceipt);
+
+      setBlockExplorerUrl(getBlockExplorerUrl(preparedTxDetails.targetChainId));
+      setFinalUserOpHash(targetReceipt.receipt.transactionHash);
+    } catch (error) {
+      console.error("Error executing crosschain user operation", error);
+    } finally {
+      setLoading(false);
+      setPreparedTxDetails(null);
     }
   }
 
@@ -91,98 +112,85 @@ export default function Main({ account, type = SignerType.WEBAUTHN }: WalletProp
       const address = await account.getAddress();
       setAccountAddress(address);
       setLoading(false);
-    })()
+    })();
   }, [account]);
 
   return (
     <div className="flex flex-col items-center justify-center gap-3 border border-gray-300 rounded-md p-6">
-      <p className="text-lg font-bold text-gray-800 dark:text-gray-200 mb-4">
-        Web3Auth Chain Abstraction Demo
-      </p>
-      {
-        loading ? (
-          <div>{loadingText}</div>
-        ) : (
-          <>
-            <div className="flex items-center justify-between gap-2 w-full">
-              <p className="text-xs bg-gray-100 p-2 rounded-md text-gray-800">
-                Smart Account: <b className="text-sm">{accountAddress}</b>
-              </p>
-              <p className="text-sm bg-red-100 p-2 rounded-md text-gray-800">
-                {type}
-              </p>
-            </div>
-            <div className="my-4 flex flex-col gap-2 w-full">
+      <p className="text-lg font-bold text-gray-800 dark:text-gray-200 mb-4">Web3Auth Chain Abstraction Demo</p>
+      {loading ? (
+        <div>{loadingText}</div>
+      ) : (
+        <>
+          <div className="flex items-center justify-between gap-2 w-full">
+            <p className="text-xs bg-gray-100 p-2 rounded-md text-gray-800">
+              Smart Account: <b className="text-sm">{accountAddress}</b>
+            </p>
+            <p className="text-sm bg-red-100 p-2 rounded-md text-gray-800">{type}</p>
+          </div>
+          <div className="my-4 flex flex-col gap-2 w-full">
+            <button className="bg-teal-500 p-2 rounded-md text-sm w-full text-white font-bold" onClick={() => setFundModalOpen(true)}>
+              Fund Test Token
+            </button>
+            <button className="bg-green-500 p-2 rounded-md text-sm w-full text-white font-bold" onClick={() => setBalanceModalOpen(true)}>
+              Show Test Token Balances
+            </button>
+            <br />
             <button
-                className="bg-teal-500 p-2 rounded-md text-sm w-full text-white font-bold"
-                onClick={() => setFundModalOpen(true)}
-              >
-                Fund Test Token
-              </button>
-              <button
-                className="bg-green-500 p-2 rounded-md text-sm w-full text-white font-bold"
-                onClick={() => setBalanceModalOpen(true)}
-              >
-                Show Test Token Balances
-              </button>
-              <br />
-              <button
-                className="bg-blue-500 p-2 rounded-md text-sm w-full text-white font-bold"
-                onClick={() => handleButtonClick(CrosschainTransactionType.CROSSCHAIN_SPONSORSHIP)}
-              >
-                Sponsor Crosschain Transaction
-              </button>
-              <button
-                className="bg-blue-500 p-2 rounded-md text-sm w-full text-white font-bold"
-                onClick={() => handleButtonClick(CrosschainTransactionType.TRANSFER_LIQUIDITY)}
-              >
-                Transfer Liquidity to Different Chain
-              </button>
-              <button
-                className="bg-blue-500 p-2 rounded-md text-sm w-full text-white font-bold"
-                onClick={() => handleButtonClick(CrosschainTransactionType.MULTI_SOURCE)}
-              >
-                Transfer Liquidity from Multiple Source Chains to Target Chain
-              </button>
+              className="bg-blue-500 p-2 rounded-md text-sm w-full text-white font-bold"
+              onClick={() => handleButtonClick(CrosschainTransactionType.CROSSCHAIN_SPONSORSHIP)}
+            >
+              Sponsor Crosschain Transaction
+            </button>
+            <button
+              className="bg-blue-500 p-2 rounded-md text-sm w-full text-white font-bold"
+              onClick={() => handleButtonClick(CrosschainTransactionType.TRANSFER_LIQUIDITY)}
+            >
+              Transfer Liquidity to Different Chain
+            </button>
+            <button
+              className="bg-blue-500 p-2 rounded-md text-sm w-full text-white font-bold"
+              onClick={() => handleButtonClick(CrosschainTransactionType.MULTI_SOURCE)}
+            >
+              Transfer Liquidity from Multiple Source Chains to Target Chain
+            </button>
+          </div>
+          {txModalOpen && (
+            <Modal>
+              <TxForm
+                type={txType}
+                onCancel={() => {
+                  setTxModalOpen(false);
+                  setPreparedTxDetails(null);
+                }}
+                onPrepare={handlePrepareTx}
+                onExecute={handleTxExecute}
+                preparedTxDetails={preparedTxDetails}
+              />
+            </Modal>
+          )}
+          {balanceModalOpen && (
+            <Modal>
+              <BalanceForm account={accountAddress} onClose={() => setBalanceModalOpen(false)} />
+            </Modal>
+          )}
+          {fundModalOpen && (
+            <Modal>
+              <FundToken accountAddress={accountAddress} onCancel={() => setFundModalOpen(false)} />
+            </Modal>
+          )}
+          {finalUserOpHash && blockExplorerUrl && (
+            <div className="mt-4 w-full bg-gray-100 p-2 rounded-md">
+              <p className="text-xs text-gray-700">
+                Target Transaction Hash:&nbsp;
+                <a className="text-xs text-blue-400 font-bold underline" target="_blank" href={`${blockExplorerUrl}/tx/${finalUserOpHash}`}>
+                  {finalUserOpHash}
+                </a>
+              </p>
             </div>
-            {
-              txModalOpen && (
-                <Modal>
-                  <TxForm type={txType} onCancel={() => setTxModalOpen(false)} onExecute={handleTxExecute} />
-                </Modal>
-              )
-            }
-            {
-              balanceModalOpen && (
-                <Modal>
-                  <BalanceForm account={accountAddress} onClose={() => setBalanceModalOpen(false)} />
-                </Modal>
-              )
-            }
-            {
-              fundModalOpen && (
-                <Modal>
-                  <FundToken accountAddress={accountAddress} onCancel={() => setFundModalOpen(false)} />
-                </Modal>
-              )
-            }
-            {finalUserOpHash && blockExplorerUrl && (
-              <div className="mt-4 w-full bg-gray-100 p-2 rounded-md">
-                <p className="text-xs text-gray-700">
-                  Target Transaction Hash:&nbsp;
-                  <a
-                    className="text-xs text-blue-400 font-bold underline"
-                    target="_blank"
-                    href={`${blockExplorerUrl}/tx/${finalUserOpHash}`}
-                  >
-                    {finalUserOpHash}
-                  </a>
-                </p>
-              </div>
-            )}
-          </>
-        )
-      }
+          )}
+        </>
+      )}
     </div>
-  )
+  );
 }
--- src/components/tx-form.tsx
+++ src/components/tx-form.tsx
@@ -10,14 +10,17 @@ import MultiSourceTx from "./multi-sourc-tx";
 interface ITxFormProps {
   type: CrosschainTransactionType;
   onCancel: () => void;
-  onExecute: (
-    sourceChainIds: number[],
-    targetChainId: number,
-    sourceFunds?: Hex[]
-  ) => void;
+  onPrepare: (sourceChainIds: number[], targetChainId: number, sourceFunds?: Hex[]) => void;
+  onExecute: () => void;
+  preparedTxDetails: {
+    estimatedGasFeesOnTargetChain: bigint;
+    totalTransactionAmountOnTargetChain: bigint;
+    sourceChainIds: number[];
+    targetChainId: number;
+  } | null;
 }
 
-export default function TxForm({ type, onCancel, onExecute }: ITxFormProps) {
+export default function TxForm({ type, onCancel, onPrepare, onExecute, preparedTxDetails }: ITxFormProps) {
   const [sourceAmount1, setSourceAmount1] = useState(0);
   const [sourceAmount2, setSourceAmount2] = useState(0);
   const [sourceChainId1, setSourceChainId1] = useState<number | undefined>(undefined);
@@ -74,20 +77,15 @@ export default function TxForm({ type, onCancel, onExecute }: ITxFormProps) {
     setTargetChainId(chainId);
   }
 
-  function handleExecute() {
+  function handlePrepareClick() {
     let sourceFunds: Hex[] | undefined;
     if (type === CrosschainTransactionType.MULTI_SOURCE) {
-      sourceFunds = [
-        toHex(parseUnits(sourceAmount1.toString(), 6)),
-        toHex(parseUnits(sourceAmount2.toString(), 6)),
-      ];
+      sourceFunds = [parseUnits(sourceAmount1.toString(), 6), parseUnits(sourceAmount2.toString(), 6)].map((n) => toHex(n));
     }
-    if (!targetChainId) {
-      return;
-    }
+    if (!targetChainId) return;
 
     const sourceChainIds = [sourceChainId1, sourceChainId2].filter((chainId) => chainId !== undefined) as number[];
-    onExecute(sourceChainIds, targetChainId, sourceFunds);
+    onPrepare(sourceChainIds, targetChainId, sourceFunds);
   }
 
   return (
@@ -97,25 +95,23 @@ export default function TxForm({ type, onCancel, onExecute }: ITxFormProps) {
         <p className="text-xs text-gray-500 whitespace-pre text-center ml-[-15px]">{getDescriptionForTxType()}</p>
       </div>
       {/** Source Chain */}
-      {
-        type === CrosschainTransactionType.MULTI_SOURCE ? (
-          <MultiSourceTx
-            handleSourceChain1Selected={handleSourceChainId1Selected}
-            handleSourceChain2Selected={handleSourceChainId2Selected}
-            sourceAmount1={sourceAmount1}
-            sourceAmount1OnChange={setSourceAmount1}
-            sourceAmount2={sourceAmount2}
-            sourceAmount2OnChange={setSourceAmount2}
-          />
-        ) : (
-          <div className="flex flex-col gap-2">
-            <p className="text-sm font-bold text-gray-900">Source Chain:</p>
-            <div className="flex gap-2">
-              <Dropdown options={[CHAIN_1, CHAIN_2, CHAIN_3]} onSelect={handleSourceChainId1Selected} />
-            </div>
+      {type === CrosschainTransactionType.MULTI_SOURCE ? (
+        <MultiSourceTx
+          handleSourceChain1Selected={handleSourceChainId1Selected}
+          handleSourceChain2Selected={handleSourceChainId2Selected}
+          sourceAmount1={sourceAmount1}
+          sourceAmount1OnChange={setSourceAmount1}
+          sourceAmount2={sourceAmount2}
+          sourceAmount2OnChange={setSourceAmount2}
+        />
+      ) : (
+        <div className="flex flex-col gap-2">
+          <p className="text-sm font-bold text-gray-900">Source Chain:</p>
+          <div className="flex gap-2">
+            <Dropdown options={[CHAIN_1, CHAIN_2, CHAIN_3]} onSelect={handleSourceChainId1Selected} />
           </div>
-        )
-      }
+        </div>
+      )}
       {/** Target Chain */}
       <div className="flex flex-col gap-2">
         <p className="text-sm font-bold text-gray-900">Target Chain:</p>
@@ -123,28 +119,41 @@ export default function TxForm({ type, onCancel, onExecute }: ITxFormProps) {
           <Dropdown options={[CHAIN_1, CHAIN_2, CHAIN_3]} onSelect={handleTargetChainSelected} />
         </div>
       </div>
-      {
-        type === CrosschainTransactionType.MULTI_SOURCE && (
-          <div className="flex items-center gap-2 bg-gray-100 p-2 rounded-md">
-            <p className="text-xs font-bold text-gray-600">
-              Transaction amount to target: <b>{formatUnits(TEST_TRANSFER_AMOUNT, 6)}&nbsp;W3P</b>
+      {type === CrosschainTransactionType.MULTI_SOURCE && (
+        <div className="flex items-center gap-2 bg-gray-100 p-2 rounded-md">
+          <p className="text-xs font-bold text-gray-600">
+            Transaction amount to target: <b>{formatUnits(TEST_TRANSFER_AMOUNT, 6)}&nbsp;W3P</b>
+          </p>
+        </div>
+      )}
+
+      {preparedTxDetails && (
+        <div className="mt-4 p-4 bg-gray-100 rounded-md">
+          <h3 className="text-lg font-bold mb-2">Transaction Details</h3>
+          <div className="space-y-2">
+            <p>Estimated Gas Fees: {preparedTxDetails.estimatedGasFeesOnTargetChain.toString()} wei</p>
+            <p>Total Transaction Amount: {preparedTxDetails.totalTransactionAmountOnTargetChain.toString()} wei</p>
+            <p>
+              Net Receivable Amount:{" "}
+              {(preparedTxDetails.totalTransactionAmountOnTargetChain - preparedTxDetails.estimatedGasFeesOnTargetChain).toString()} wei
             </p>
           </div>
-        )
-      }
-      <div className="flex gap-2">
-        <button
-          className="bg-blue-500 p-2 rounded-md text-sm w-full text-white font-bold"
-          onClick={onCancel}
-        >
+        </div>
+      )}
+
+      <div className="flex justify-end gap-2 mt-4">
+        <button type="button" onClick={onCancel} className="bg-blue-500 p-2 rounded-md text-sm w-full text-white font-bold">
           Cancel
         </button>
-        <button
-          className="bg-blue-500 p-2 rounded-md text-sm w-full text-white font-bold"
-          onClick={handleExecute}
-        >
-          Execute
-        </button>
+        {!preparedTxDetails ? (
+          <button type="button" onClick={handlePrepareClick} className="bg-blue-500 p-2 rounded-md text-sm w-full text-white font-bold">
+            Prepare Transaction
+          </button>
+        ) : (
+          <button type="button" onClick={onExecute} className="bg-blue-500 p-2 rounded-md text-sm w-full text-white font-bold">
+            Execute Transaction
+          </button>
+        )}
       </div>
     </div>
   );
